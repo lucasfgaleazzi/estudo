4d38a1146618e7cd5e113e320e09f7d3637b0ffb
lucasfgaleazzi <116013533+lucasfgaleazzi@users.noreply.github.com>
2022-10-17 14:39:06 -0300

    Add files via upload

---
+++ desafio_area.js
@@ -0,0 +1,9 @@
+// PI * raio * raio 
+// PI = 3.141592
+
+let raio = 10;
+const PI = 3.141592;
+let areaCirc = PI * raio * raio;
+
+console.log("Resultado da área = " + areaCirc + " m2.");
+console.log(Math.PI);
